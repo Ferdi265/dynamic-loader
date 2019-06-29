39ff4e23acbc56fe66b9ddeafb038a89b404b0a7
Ferdinand Bachmann <theferdi265@gmail.com>
2019-06-29 18:10:30 +0200

    fix test cmake files

--- tests/common/start.c
+++ tests/common/start.c
@@ -1,13 +1,13 @@
 #include "syscall.h"
 #include "crt.h"
 
-void _init() {}
-void _fini() {}
-
 __attribute__((noreturn))
 int __libc_start_main(mainfn * main, int argc, char ** argv, voidfn * init, voidfn * fini, void (*rtld_fini)(void), void * stack_end) {
     char ** envp = &argv[argc + 1];
+
+    init();
     int ret = main(argc, argv, envp);
+    fini();
 
     __syscall(SYS_exit, ret, 0, 0, 0, 0, 0);
     __builtin_unreachable();
