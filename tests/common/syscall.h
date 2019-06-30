dd860cb25028d63357e7e725565b7c4e1e0dc948
Ferdinand Bachmann <theferdi265@gmail.com>
2019-06-30 23:01:23 +0200

    make __syscall no longer be hidden

--- tests/common/syscall.h
+++ tests/common/syscall.h
@@ -3,5 +3,4 @@
 #include <stddef.h>
 #include <sys/syscall.h>
 
-__attribute__((weak, visibility("hidden")))
 size_t __syscall(size_t nr, size_t a0, size_t a1, size_t a2, size_t a3, size_t a4, size_t a5);
