9a7c271f07331104dacc08292c72a46f98d5e587
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2025-05-07 18:27:29 +0200

    loader/init.c: fix format warnings

--- loader/init.c
+++ loader/init.c
@@ -8,7 +8,7 @@ void dso_initialize(dso_t * dso) {
     dso_ref(dso);
     voidfn_t * init = dyn_get_init(dso->dyn, dso->base);
     if (init != NULL && !env_noexec) {
-        DEBUG(init, "Calling initializer INIT at %p\n", init);
+        DEBUG(init, "Calling initializer INIT at %p\n", (void *)init);
 
         pthread_mutex_unlock(&dso_lock);
         init();
@@ -21,7 +21,7 @@ void dso_initialize(dso_t * dso) {
         DEBUG(init, "Processing INIT_ARRAY of length %zd\n", init_array_length);
         for (size_t i = 0; i < init_array_length; i++) {
             if (!env_noexec) {
-                DEBUG(init, "Calling initializer INIT_ARRAY[%zd] at %p\n", i, init_array[i]);
+                DEBUG(init, "Calling initializer INIT_ARRAY[%zd] at %p\n", i, (void *)init_array[i]);
 
                 pthread_mutex_unlock(&dso_lock);
                 init_array[i]();
@@ -40,7 +40,7 @@ void dso_finalize(dso_t * dso) {
     dso_ref(dso);
     voidfn_t * fini = dyn_get_fini(dso->dyn, dso->base);
     if (fini != NULL && !env_noexec) {
-        DEBUG(init, "Calling finalizer FINI at %p\n", fini);
+        DEBUG(init, "Calling finalizer FINI at %p\n", (void *)fini);
 
         pthread_mutex_unlock(&dso_lock);
         fini();
@@ -53,7 +53,7 @@ void dso_finalize(dso_t * dso) {
         DEBUG(init, "Processing FINI_ARRAY of length %zd\n", fini_array_length);
         for (size_t i = 0; i < fini_array_length; i++) {
             if (!env_noexec) {
-                DEBUG(init, "Calling finalizer FINI_ARRAY[%zd] at %p\n", i, fini_array[i]);
+                DEBUG(init, "Calling finalizer FINI_ARRAY[%zd] at %p\n", i, (void *)fini_array[i]);
 
                 pthread_mutex_unlock(&dso_lock);
                 fini_array[i]();
